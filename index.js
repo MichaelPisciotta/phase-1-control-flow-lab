ffdb64e07a6af00507b322cbed16c4229bc0d76d
MichaelPisciotta <mfp56@miami.edu>
2021-07-20 17:49:08 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue < 400){
+    return "This one is on me!"
+  } else if (someValue === 2001) {
+    return "I will gladly take your thirty bucks."
+  } else if (someValue === 2501) {
+    return "No can do."
+  }
+}
+scuberGreetingForFeet(199);
+
+
+
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? ("Ok, sounds good.") : ('No go.');
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function switchOnCharmFromTip(tip){
+
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+    return 'Thank you.';
+    break;
+    case 'thanks for everything':
+      return 'Bye.';
+    break;
+  }
+  
+  } 
+
+
+
+
+
+
+
